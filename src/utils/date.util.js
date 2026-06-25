edd680c65797d48f0e826be5e0da289f4c584b78
gwenthinguyenngoc12 <gwen.nguyen91296@gmail.com>
2026-06-25 16:28:12 -0400

    updater date

--- src/utils/date.util.js
+++ src/utils/date.util.js
@@ -3,3 +3,26 @@ export function getTodayDate() {
     timeZone: "America/Toronto",
   });
 }
+
+export function formatVietnameseDateTime(dateString = "") {
+  if (!dateString) {
+    return "Không rõ thời gian";
+  }
+
+  const date = new Date(dateString);
+
+  if (Number.isNaN(date.getTime())) {
+    return "Không rõ thời gian";
+  }
+
+  return new Intl.DateTimeFormat("vi-VN", {
+    timeZone: "Asia/Ho_Chi_Minh",
+    weekday: "long",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(date);
+}
