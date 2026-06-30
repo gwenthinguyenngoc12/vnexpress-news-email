4488a46313653f1c2e13656e1f43a397b64e4a74
gwenthinguyenngoc12 <gwen.nguyen91296@gmail.com>
2026-06-29 23:27:53 -0400

    Update unit test for date

--- tests/utils/text.util.test.js
+++ tests/utils/text.util.test.js
@@ -7,22 +7,22 @@ describe (" cleanHtmlText",() => {
         const result = cleanHtmlText(html);
         expect(result).toBe("Đây là mô tả bài viết.");
 
-    })
+    });
 
     it("should replace HTML entities with readable characters", () => {
         const html = 'Tin&nbsp;tức &amp; đời sống &quot;hôm nay&quot;';
         const result = cleanHtmlText(html);
         expect(result).toBe('Tin tức & đời sống "hôm nay"');
-    })
+    });
 
     it("should return empty string when input is empty", () => {
         const result = cleanHtmlText("");
         expect(result).toBe("");
-    })
+    });
 
     it("should return empty string when input undifined", () => {
         const result = cleanHtmlText();
         expect(result).toBe("");
-    })
-})
+    });
+});
 
